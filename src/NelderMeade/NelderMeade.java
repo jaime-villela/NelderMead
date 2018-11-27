64d888072992152f119b654ca1e7c38ac142f181
jvillela <jaime.villela@gmail.com>
2018-11-26 21:45:18 -0600

    Adding function interface

--- src/NelderMeade/NelderMeade.java
+++ src/NelderMeade/NelderMeade.java
@@ -1,19 +1,23 @@
 package NelderMeade;
 
+@FunctionalInterface
+interface PolyFunc {
+    double calculate(double[] row);
+}
+
 public class NelderMeade {
-    private int numRows;
     private int numCols;
     private double[][] matrix;
+    PolyFunc func;
 
     public NelderMeade() {}
 
     public void setMatrix(double[][] matrix) {
         this.matrix = matrix;
-        this.numRows = matrix.length;
         this.numCols = matrix[0].length;
     }
 
-    public double[] addRows(int row1, int row2) {
+    private double[] addRows(int row1, int row2) {
         double[] retVal = new double[this.numCols];
 
         for (int i = 0; i < this.numCols; i++)
@@ -22,7 +26,7 @@ public class NelderMeade {
         return retVal;
     }
 
-    public double[] divByN(double[] row, int divisor) {
+    private double[] divByN(double[] row, int divisor) {
         double[] retVal = new double[this.numCols];
 
         try {
@@ -36,4 +40,32 @@ public class NelderMeade {
 
         return retVal;
     }
+
+    public double[] calcMidpoint(int row1, int row2) {
+        return divByN(addRows(row1, row2), 2);
+    }
+
+    private double[] subtractRows(int row1, int row2) {
+        double[] retVal = new double[this.numCols];
+
+        for (int i = 0; i < this.numCols; i++)
+            retVal[i] = matrix[row1][i] - matrix[row2][i];
+
+        return retVal;
+    }
+
+    private double[] multByN(double[] row, int multiplier) {
+        double[] retVal = new double[this.numCols];
+
+        for (int i = 0; i < this.numCols; i++)
+            retVal[i] = row[i] * multiplier;
+
+        return retVal;
+    }
+
+    public double evalFuncOnRow(int row) {
+        return this.func.calculate(this.matrix[row]);
+    }
+
+
 }
--- src/NelderMeade/NelderMeadeTest.java
+++ src/NelderMeade/NelderMeadeTest.java
@@ -3,6 +3,8 @@ package NelderMeade;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.format.DecimalStyle;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NelderMeadeTest {
@@ -18,15 +20,22 @@ public class NelderMeadeTest {
     public void setUp() {
         nm = new NelderMeade();
         nm.setMatrix(matrix);
+        nm.func = (double[] row)->Math.pow(row[0],2) - 4*row[0] + Math.pow(row[1], 2) - row[1] - row[0]*row[1];
     }
 
     @Test
-    public void testAddTwoRows() throws Exception {
-        assertArrayEquals(nm.addRows(0,1), new double[]{1.2,0.0});
+    public void testCalcMidpoint() throws Exception {
+        assertArrayEquals(nm.calcMidpoint(0,1), new double[]{0.6, 0.0});
     }
 
     @Test
-    public void testDivByN() throws Exception {
-        assertArrayEquals(nm.divByN(nm.addRows(0,1), 2), new double[]{0.6, 0.0});
+    public void evalFuncAtV2() throws Exception {
+        assertEquals(nm.evalFuncOnRow(1), -3.36);
+    }
+
+    @Test
+    public void evalFuncAtV3() throws Exception {
+        // Unfortunately, I have to play tricks with rounding to get this test to pass.
+        assertEquals((double) Math.round(nm.evalFuncOnRow(2) * 100)/100, -0.16);
     }
 }
